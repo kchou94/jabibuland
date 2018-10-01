9c392ff24e872d396ae1d57abf1a2987da8f0385
Kevin Chou <kchou94@gmail.com>
2018-10-01 00:37:50 -0400

    start passport stuff

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var mongoose = require('mongoose');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -16,12 +18,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // mongoose setup
-var mongoDB = 'mongodb://localhost:27017';
+var mongoDB = 'mongodb://localhost/jabibuland';
 mongoose.connect(mongoDB);
-mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+if (process.env.NODE_ENV != 'production'){
+  db.once('open', function(){
+    console.log('CONNECTED to MongoDB');
+  });
+}
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/login', passport.authenticate('local' ,{
+  successRedirect: '/success',
+  failureRedirect: '/',
+  failureFlash: true
+})
+);
+
+router.get('/success', function(req, res){
+  res.send('Success!');
+});
+
 module.exports = router;
